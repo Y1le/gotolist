efd44824a5368468f0d6a1fb54d0c40867df555d
liam <2430962371@qq.com>
2026-06-20 11:09:21 +0800

    chore(cmd,docs): start EventBus worker in main and refresh Postman collection
    - cmd/main.go: launch in-proc EventBus worker goroutine, drop debug prints
    - docs/.../postman_collection.json: full 9-endpoint collection with auto-token
      capture via test script and base_url/token variables

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"context"
+
 	config "github.com/CocaineCong/todolist-ddd/conf"
 	"github.com/CocaineCong/todolist-ddd/infrastructure/common/log"
 	"github.com/CocaineCong/todolist-ddd/infrastructure/container"
+	"github.com/CocaineCong/todolist-ddd/infrastructure/eventbus"
 	"github.com/CocaineCong/todolist-ddd/infrastructure/persistence/dbs"
 	"github.com/CocaineCong/todolist-ddd/interfaces/adapter/initialize"
 )
 
 func main() {
 	loadingInfra()
+
+	bus := eventbus.NewInProcBus(dbs.DB)
+	container.LoadingDomain(bus)
+	go bus.Start(context.Background())
+
 	r := initialize.NewRouter()
-	_ = r.Run(config.Conf.Server.Port)
+	addr := ":3001"
+	if config.Conf != nil && config.Conf.Server != nil && config.Conf.Server.Port != "" {
+		addr = config.Conf.Server.Port
+	}
+	_ = r.Run(addr)
 }
 
-// loadingInfra 加载基础架构信息
 func loadingInfra() {
 	config.InitConfig()
 	log.InitLog()
 	dbs.MySQLInit()
-
-	container.LoadingDomain()
 }
